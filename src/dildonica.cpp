91ac35b302bdf32d419bc8a61f0760ac96f18b63
Fletcher Dostie <fdostie@gmail.com>
2024-07-30 20:19:00 -0700

    Add BLE MIDI!

--- src/dildonica.cpp
+++ src/dildonica.cpp
@@ -16,9 +16,10 @@
 #include "dildonica.hpp"
 
 
-
-static nrfx_timer_t TIMER1 = NRFX_TIMER_INSTANCE(1);
-static nrfx_timer_t TIMER2 = NRFX_TIMER_INSTANCE(2);
+#define D_TIMER_INST_IDX 3
+#define D_COUNTER_INST_IDX 4
+static nrfx_timer_t TIMER_D_TIMER = NRFX_TIMER_INSTANCE(D_TIMER_INST_IDX);
+static nrfx_timer_t TIMER_D_COUNTER = NRFX_TIMER_INSTANCE(D_COUNTER_INST_IDX);
 
 struct DildonicaSampleRaw {
     uint8_t zone;
@@ -35,10 +36,11 @@ struct DildonicaZoneState {
 
 #define ZEPHYR_USER_NODE DT_PATH(zephyr_user)
 
+#define LED_ON 1
+#define LED_OFF 0
 static const struct gpio_dt_spec PIN_LED0 = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
-static const struct gpio_dt_spec PIN_LED1 = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
-static const struct gpio_dt_spec PIN_LED2 = GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
-static const struct gpio_dt_spec PIN_LED3 = GPIO_DT_SPEC_GET(DT_ALIAS(led3), gpios);
+//static const struct gpio_dt_spec PIN_LED1 = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
+//static const struct gpio_dt_spec PIN_LED2 = GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
 // const int PIN_RGBLED_CLOCK = 6;
 // const int PIN_RGBLED_DATA = 8;
 //static const struct gpio_dt_spec PIN_DILDONICA_OSC = GPIO_DT_SPEC_GET(ZEPHYR_USER_NODE, dildonica_osc_in_gpios);
@@ -56,21 +58,21 @@ static const struct gpio_dt_spec PIN_DILDONICA_ZONE_SELECT[] = {
 
 // Timer begins counting after this many oscillation cycles
 // The delay is to ignore irregular cycles right after the coil is powered up
-const uint32_t DILDONICA_MEASUREMENT_CYCLES_BEGIN = 32;
+const uint32_t DILDONICA_MEASUREMENT_CYCLES_BEGIN = 100;
 // Measurement stops after this many cycles
 const uint32_t DILDONICA_MEASUREMENT_CYCLES_END = 10000 + DILDONICA_MEASUREMENT_CYCLES_BEGIN;
 // If we don't have enough cycles after this much time, timeout - hardware error (coil broken, etc.)
-const uint32_t DILDONICA_MEASUREMENT_TIMEOUT_US = 20000;
+const uint32_t DILDONICA_MEASUREMENT_TIMEOUT_US = 50000;
 
 
 // Low and high voltage threshold values used for counting cycles
 const uint32_t DILDONICA_OSC_COMP_THRESH_LO = 10;
 const uint32_t DILDONICA_OSC_COMP_THRESH_HI = 20;
 
-const uint8_t DILDONICA_N_ZONES = 8;
+const uint8_t DILDONICA_N_ZONES = 1;
 uint8_t dildonicaCurZone = 0;
 
-CircularQueue<DildonicaSampleRaw, 256> dildonicaSampleQueue;
+static CircularQueue<DildonicaSampleRaw, 256> dildonicaSampleQueue;
 
 const float DILDONICA_SAMPLE_MAX_ERROR = 10000;
 const float DILDONICA_SAMPLE_AVERAGE_COEFF = 0.001;
@@ -95,10 +97,10 @@ char ( &_ArraySizeHelper( T (&array)[N] ))[N];
 #define UARTE_INST_IDX 1
 
 /** @brief Symbol specifying TX pin number of UARTE. */
-#define UARTE_TX_PIN NRF_GPIO_PIN_MAP(0,3)
+#define UARTE_TX_PIN NRF_GPIO_PIN_MAP(0,24)
 
 /** @brief Symbol specifying RX pin number of UARTE. */
-#define UARTE_RX_PIN NRF_GPIO_PIN_MAP(0,28)
+#define UARTE_RX_PIN NRF_GPIO_PIN_MAP(0,25)
 
 nrfx_uarte_t uarte_inst = NRFX_UARTE_INSTANCE(UARTE_INST_IDX);
 
@@ -135,15 +137,25 @@ void printf_uart( const char* format, ... ) {
 
 void setDildonicaZoneActiveMask(uint8_t zoneMask) {
     for (uint8_t i = 0; i != DILDONICA_N_ZONES; i++) {
-        bool pinState = ((1 << i) &
-        zoneMask);
+        bool pinState = ((1 << i) & zoneMask);
         gpio_pin_set_dt(&PIN_DILDONICA_ZONE_SELECT[i], pinState);
     }
 }
 
+void updateDildonicaZone(DildonicaZoneState& zoneState, DildonicaSampleRaw& sample) {
+    float cyclePeriod = (float)sample.cyclePeriod;
+    zoneState.valueNormalized = cyclePeriod - zoneState.cyclePeriodExponentialMean;
+    if( fabs(zoneState.valueNormalized) > ExponentialMeanMaxError ) {
+        zoneState.cyclePeriodExponentialMean = cyclePeriod;
+        zoneState.valueNormalized = 0;
+    } else {
+        zoneState.cyclePeriodExponentialMean += zoneState.valueNormalized * ExponentialMeanRate;
+    }
+    zoneState.valueNormalized /= zoneState.cyclePeriodExponentialMean;
+}
 
-static void timer1_handler(nrf_timer_event_t event_type, void* p_context) {
-    if (event_type == NRF_TIMER_EVENT_COMPARE2) {
+static void d_timer_handler(nrf_timer_event_t event_type, void* p_context) {
+    if (event_type == NRF_TIMER_EVENT_COMPARE0) {
         // Measurement timeout - probably coil problem.
 
         DildonicaSampleRaw thisSample = {
@@ -155,29 +167,26 @@ static void timer1_handler(nrf_timer_event_t event_type, void* p_context) {
         dildonicaCurZone = (dildonicaCurZone + 1) % DILDONICA_N_ZONES;
         setDildonicaZoneActiveMask(1 << dildonicaCurZone);
 
-        nrfx_timer_clear(&TIMER1);
-        nrfx_timer_clear(&TIMER2);
+        nrfx_timer_clear(&TIMER_D_TIMER);
+        nrfx_timer_clear(&TIMER_D_COUNTER);
     }
 }
 
-static void timer2_handler(nrf_timer_event_t event_type, void* p_context) {
+static void d_counter_handler(nrf_timer_event_t event_type, void* p_context) {
     if (event_type == NRF_TIMER_EVENT_COMPARE1) {
         // Normal coil measurement
 
         DildonicaSampleRaw thisSample = {
             dildonicaCurZone,
-            nrfx_timer_capture_get(&TIMER1, NRF_TIMER_CC_CHANNEL1) - nrfx_timer_capture_get(&TIMER1, NRF_TIMER_CC_CHANNEL0)
+            nrfx_timer_capture_get(&TIMER_D_TIMER, NRF_TIMER_CC_CHANNEL1) - nrfx_timer_capture_get(&TIMER_D_TIMER, NRF_TIMER_CC_CHANNEL0)
         };
         dildonicaSampleQueue.enqueue(thisSample);
 
         dildonicaCurZone = (dildonicaCurZone + 1) % DILDONICA_N_ZONES;
         setDildonicaZoneActiveMask(1 << dildonicaCurZone);
 
-        // size_t len = sprintf((char*)uart_tx_buf, "Hello dildonica");
-        // uart_tx(uart, uart_tx_buf, len, SYS_FOREVER_US);
-
-        nrfx_timer_clear(&TIMER1);
-        nrfx_timer_clear(&TIMER2);
+        nrfx_timer_clear(&TIMER_D_TIMER);
+        nrfx_timer_clear(&TIMER_D_COUNTER);
     }
 }
 
@@ -191,7 +200,7 @@ void setup_comparator() {
                 nrfx_comp_irq_handler, 0, 0);
 #endif
 
-    nrfx_comp_config_t comp_config = NRFX_COMP_DEFAULT_CONFIG(NRF_COMP_INPUT_2);
+    nrfx_comp_config_t comp_config = NRFX_COMP_DEFAULT_CONFIG(NRF_COMP_INPUT_4);
     comp_config.main_mode = NRF_COMP_MAIN_MODE_SE;
     comp_config.speed_mode = NRF_COMP_SP_MODE_HIGH;
     comp_config.reference = NRF_COMP_REF_VDD;
@@ -205,28 +214,28 @@ void setup_comparator() {
 void setup_ppi() {
     nrf_ppi_channel_t ppi_channel;
 
-    // Comparator UP -> Timer2 Count
+    // Comparator UP -> TIMER_D_COUNTER Count
     nrfx_ppi_channel_alloc(&ppi_channel);
-    nrfx_ppi_channel_assign(ppi_channel, nrfx_comp_event_address_get(NRF_COMP_EVENT_UP), nrfx_timer_task_address_get(&TIMER2, NRF_TIMER_TASK_COUNT));
+    nrfx_ppi_channel_assign(ppi_channel, nrfx_comp_event_address_get(NRF_COMP_EVENT_UP), nrfx_timer_task_address_get(&TIMER_D_COUNTER, NRF_TIMER_TASK_COUNT));
     nrfx_ppi_channel_enable(ppi_channel);
 
-    // Timer2 Compare -> Timer1 Capture
+    // TIMER_D_COUNTER Compare -> Timer1 Capture
     nrfx_ppi_channel_alloc(&ppi_channel);
-    nrfx_ppi_channel_assign(ppi_channel, nrfx_timer_event_address_get(&TIMER2, NRF_TIMER_EVENT_COMPARE0), nrfx_timer_task_address_get(&TIMER1, NRF_TIMER_TASK_CAPTURE0));
+    nrfx_ppi_channel_assign(ppi_channel, nrfx_timer_event_address_get(&TIMER_D_COUNTER, NRF_TIMER_EVENT_COMPARE0), nrfx_timer_task_address_get(&TIMER_D_TIMER, NRF_TIMER_TASK_CAPTURE0));
     nrfx_ppi_channel_enable(ppi_channel);
 
     nrfx_ppi_channel_alloc(&ppi_channel);
-    nrfx_ppi_channel_assign(ppi_channel, nrfx_timer_event_address_get(&TIMER2, NRF_TIMER_EVENT_COMPARE1), nrfx_timer_task_address_get(&TIMER1, NRF_TIMER_TASK_CAPTURE1));
+    nrfx_ppi_channel_assign(ppi_channel, nrfx_timer_event_address_get(&TIMER_D_COUNTER, NRF_TIMER_EVENT_COMPARE1), nrfx_timer_task_address_get(&TIMER_D_TIMER, NRF_TIMER_TASK_CAPTURE1));
     nrfx_ppi_channel_enable(ppi_channel);
 }
 
 void setup_gpio() {
     gpio_pin_configure_dt(&PIN_LED0, GPIO_OUTPUT);
-    gpio_pin_set_dt(&PIN_LED0, 0);
-    gpio_pin_configure_dt(&PIN_LED1, GPIO_OUTPUT);
-    gpio_pin_set_dt(&PIN_LED1, 0);
-    gpio_pin_configure_dt(&PIN_LED2, GPIO_OUTPUT);
-    gpio_pin_set_dt(&PIN_LED2, 0);
+    //gpio_pin_configure_dt(&PIN_LED1, GPIO_OUTPUT);
+    //gpio_pin_configure_dt(&PIN_LED2, GPIO_OUTPUT);
+    gpio_pin_set_dt(&PIN_LED0, LED_OFF);
+    //gpio_pin_set_dt(&PIN_LED1, LED_OFF);
+    //gpio_pin_set_dt(&PIN_LED2, LED_OFF);
     //gpio_pin_configure_dt(&PIN_DILDONICA_OSC, GPIO_INPUT);
 
     for (int i = 0; i < mycountof(PIN_DILDONICA_ZONE_SELECT); i++) {
@@ -239,40 +248,44 @@ void setup_gpio() {
 void setup_timers() {
 
 #if defined(__ZEPHYR__)
-    IRQ_CONNECT(NRFX_IRQ_NUMBER_GET(NRF_TIMER_INST_GET(1)), 0,
-                NRFX_TIMER_INST_HANDLER_GET(1), 0, 0);
-    IRQ_CONNECT(NRFX_IRQ_NUMBER_GET(NRF_TIMER_INST_GET(2)), 0,
-                NRFX_TIMER_INST_HANDLER_GET(2), 0, 0);
+    IRQ_CONNECT(NRFX_IRQ_NUMBER_GET(NRF_TIMER_INST_GET(D_TIMER_INST_IDX)), 0,
+                NRFX_TIMER_INST_HANDLER_GET(D_TIMER_INST_IDX), 0, 0);
+    IRQ_CONNECT(NRFX_IRQ_NUMBER_GET(NRF_TIMER_INST_GET(D_COUNTER_INST_IDX)), 0,
+                NRFX_TIMER_INST_HANDLER_GET(D_COUNTER_INST_IDX), 0, 0);
 #endif
 
-    uint32_t base_frequency = NRF_TIMER_BASE_FREQUENCY_GET(TIMER1.p_reg);
+    uint32_t base_frequency = NRF_TIMER_BASE_FREQUENCY_GET(TIMER_D_TIMER.p_reg);
     nrfx_timer_config_t timer_cfg = NRFX_TIMER_DEFAULT_CONFIG(base_frequency);
     timer_cfg.bit_width = NRF_TIMER_BIT_WIDTH_32;
     timer_cfg.p_context = (void*)"Some context";
 
-    nrfx_timer_init(&TIMER1, &timer_cfg, timer1_handler);
+    nrfx_timer_init(&TIMER_D_TIMER, &timer_cfg, d_timer_handler);
     // Timeout interrupt
-    nrfx_timer_extended_compare(&TIMER1,
+    nrfx_timer_extended_compare(&TIMER_D_TIMER,
                                 NRF_TIMER_CC_CHANNEL2,
                                 DILDONICA_MEASUREMENT_TIMEOUT_US * 16,
                                 (nrf_timer_short_mask_t)0,
                                 true);
 
-    timer_cfg.mode = NRF_TIMER_MODE_COUNTER;
-    nrfx_timer_init(&TIMER2, &timer_cfg, timer2_handler);
-    nrfx_timer_extended_compare(&TIMER2,
+    base_frequency = NRF_TIMER_BASE_FREQUENCY_GET(TIMER_D_COUNTER.p_reg);
+    nrfx_timer_config_t counter_cfg = NRFX_TIMER_DEFAULT_CONFIG(base_frequency);
+    counter_cfg.bit_width = NRF_TIMER_BIT_WIDTH_32;
+    counter_cfg.p_context = (void*)"Some context";
+    counter_cfg.mode = NRF_TIMER_MODE_COUNTER;
+    nrfx_timer_init(&TIMER_D_COUNTER, &counter_cfg, d_counter_handler);
+    nrfx_timer_extended_compare(&TIMER_D_COUNTER,
                                 NRF_TIMER_CC_CHANNEL0,
                                 DILDONICA_MEASUREMENT_CYCLES_BEGIN,
                                 (nrf_timer_short_mask_t)0,
                                 false);
-    nrfx_timer_extended_compare(&TIMER2,
+    nrfx_timer_extended_compare(&TIMER_D_COUNTER,
                                 NRF_TIMER_CC_CHANNEL1,
                                 DILDONICA_MEASUREMENT_CYCLES_END,
                                 (nrf_timer_short_mask_t)0,
                                 true);
 
-    nrfx_timer_enable(&TIMER1);
-    nrfx_timer_enable(&TIMER2);
+    nrfx_timer_enable(&TIMER_D_TIMER);
+    nrfx_timer_enable(&TIMER_D_COUNTER);
 }
 
 
@@ -287,25 +300,21 @@ int main(void) {
 
     printf_uart("Hello Dildonica\r\n");
 
-    // //setupMidi();  // Assuming this function is refactored to use nrfx as well
+    // setupMidi();
 
     while (1) {
         if (!dildonicaSampleQueue.is_empty()) {
+
             DildonicaSampleRaw dSample = dildonicaSampleQueue.dequeue();
 
-            gpio_pin_set_dt(&PIN_LED0, dSample.cyclePeriod & 1);
+            DildonicaZoneState& zoneState = dildonicaZoneStates[dSample.zone];
+            updateDildonicaZone(zoneState, dSample);
 
-            uint8_t dSampleZone = dSample.zone;
+            //gpio_pin_set_dt(&PIN_LED0, dSample.cyclePeriod & 1);
+            gpio_pin_set_dt(&PIN_LED0, (led_test++) & 1);
 
-            printf_uart("%d %d\r\n", dSampleZone, dSample.cyclePeriod);
-            // DildonicaZoneState& zoneState = dildonicaZoneStates[dSampleZone];
-
-
-            // updateDildonicaZone(zoneState, dSample);
-
-            // static char message[64];
-            // size_t message_len = sprintf(message, "%d, %d, %0.9f\r\n", dSampleZone, dSample.cyclePeriod, zoneState.valueNormalized);
-            // serialWrite((uint8_t *) message, message_len);
+            //printf_uart("%d, %d, %0.9f\r\n", dSample.zone, dSample.cyclePeriod, zoneState.valueNormalized);
+            printf_uart("%d, %d\r\n", dSample.zone, dSample.cyclePeriod);
 
             // int32_t midiControlValue = lround(zoneState.valueNormalized * DILDONICA_MIDI_CONTROL_SLOPE) + 63;
             // midiControlValue = max(0, midiControlValue);
@@ -323,8 +332,6 @@ int main(void) {
             //     rgbLed.show();
             // }
         }
-
-
     }
 }
 
---
+++ src/peripheral_gatt_write_midi.c
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/gatt.h>
+
+extern int mtu_exchange(struct bt_conn *conn);
+extern int write_cmd(struct bt_conn *conn);
+extern struct bt_conn *conn_connected;
+extern uint32_t last_write_rate;
+
+static uint8_t midi_data[5]; // Buffer for MIDI messages
+
+#define BT_UUID_MIDI_SERVICE BT_UUID_128_ENCODE(0x03B80E5A, 0xEDE8, 0x4B33, 0xA751, 0x6CE34EC4C700)
+static struct bt_uuid_128 midi_service_uuid = BT_UUID_INIT_128(BT_UUID_MIDI_SERVICE);
+
+#define BT_UUID_MIDI_CHARACTERISTIC BT_UUID_128_ENCODE(0x7772E5DB, 0x3868, 0x4112, 0xA1A9, 0xF2669D106BF3)
+static struct bt_uuid_128 midi_characteristic_uuid = BT_UUID_INIT_128(BT_UUID_MIDI_CHARACTERISTIC);
+
+static void midi_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value)
+{
+    bool notif_enabled = (value == BT_GATT_CCC_NOTIFY);
+    printk("MIDI notifications %s\n", notif_enabled ? "enabled" : "disabled");
+}
+
+static ssize_t read_midi(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+                         void *buf, uint16_t len, uint16_t offset)
+{
+    printk("MIDI read request\n");
+    return bt_gatt_attr_read(conn, attr, buf, len, offset, midi_data, sizeof(midi_data));
+}
+
+BT_GATT_SERVICE_DEFINE(midi_svc,
+    BT_GATT_PRIMARY_SERVICE(&midi_service_uuid),
+    BT_GATT_CHARACTERISTIC(&midi_characteristic_uuid.uuid,
+                           BT_GATT_CHRC_READ | BT_GATT_CHRC_WRITE | BT_GATT_CHRC_WRITE_WITHOUT_RESP | BT_GATT_CHRC_NOTIFY,
+                           BT_GATT_PERM_READ | BT_GATT_PERM_WRITE,
+                           read_midi, NULL, midi_data),
+	BT_GATT_CCC(midi_ccc_cfg_changed, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
+
+);
+
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+    BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_MIDI_SERVICE),
+};
+
+static const struct bt_data sd[] = {
+	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1),
+};
+
+
+
+static void mtu_updated(struct bt_conn *conn, uint16_t tx, uint16_t rx)
+{
+	printk("Updated MTU: TX: %d RX: %d bytes\n", tx, rx);
+}
+
+#if defined(CONFIG_BT_SMP)
+static void auth_cancel(struct bt_conn *conn)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	printk("Pairing cancelled: %s\n", addr);
+}
+
+static struct bt_conn_auth_cb auth_callbacks = {
+	.cancel = auth_cancel,
+};
+#endif /* CONFIG_BT_SMP */
+
+static struct bt_gatt_cb gatt_callbacks = {
+	.att_mtu_updated = mtu_updated
+};
+
+static void send_midi_control_change(uint16_t timestamp, uint8_t channel, uint8_t controller, uint8_t value)
+{
+	struct bt_conn *conn = NULL;
+
+	if (conn_connected) {
+		/* Get a connection reference to ensure that a
+			* reference is maintained in case disconnected
+			* callback is called while we perform GATT Write
+			* command.
+			*/
+		conn = bt_conn_ref(conn_connected);
+	} else {
+        printk("Not connected\n");
+        return;
+    }
+
+	if (conn) {
+		// Construct MIDI message
+		midi_data[0] = 0x80 | ((timestamp >> 7) & 0b0111111);
+		midi_data[1] = 0x80 | ((timestamp) 		& 0b1111111);
+		midi_data[2] = 0xB0 | (channel & 0x0F); // Control Change status byte
+		midi_data[3] = controller & 0x7F; // Controller number
+		midi_data[4] = value & 0x7F; // Controller value
+
+		// Send MIDI message as a notification
+		// int err = bt_gatt_write_without_response(conn_connected, midi_svc.attrs[1].handle, midi_data, sizeof(midi_data), false);
+		int err = bt_gatt_notify(conn_connected, &midi_svc.attrs[1], midi_data, sizeof(midi_data));
+		if (err) {
+			printk("Failed to send MIDI notification (err %d)\n", err);
+		} else {
+			printk("Sent MIDI Control Change Notification: Channel %d, Controller %d, Value %d\n", channel, controller, value);
+		}
+
+		bt_conn_unref(conn);
+	} else {
+        printk("Not connected\n");
+        return;
+    }
+}
+
+void setup_bluetooth_peripheral()
+{
+	int err;
+
+	err = bt_enable(NULL);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	bt_gatt_cb_register(&gatt_callbacks);
+
+#if defined(CONFIG_BT_SMP)
+	(void)bt_conn_auth_cb_register(&auth_callbacks);
+#endif /* CONFIG_BT_SMP */
+
+	err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+
+	printk("Advertising successfully started\n");
+
+	conn_connected = NULL;
+}
